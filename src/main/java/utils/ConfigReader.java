9f7933dd16040c09b86e66617a8f9815961461d4
annasolyannikova57 <60076564+stray322@users.noreply.github.com>
2025-03-31 15:13:54 +0400

    Update ConfigReader.java

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -14,11 +14,11 @@ public class ConfigReader {
     static {
         try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
             if (input == null) {
-                throw new RuntimeException("Файл config.properties не найден!");
+                throw new RuntimeException(String.format("Файл %s не найден!", "config.properties"));
             }
             properties.load(input);
         } catch (IOException e) {
-            throw new RuntimeException("Ошибка чтения config.properties", e);
+            throw new RuntimeException(String.format("Ошибка чтения файла: %s", "config.properties"), e);
         }
     }
 
@@ -30,7 +30,7 @@ public class ConfigReader {
     public static String getProperty(String key) {
         String value = properties.getProperty(key);
         if (value == null) {
-            throw new RuntimeException("Свойство " + key + " не найдено в config.properties!");
+            throw new RuntimeException(String.format("Свойство %s не найдено в config.properties!", key));
         }
         return value;
     }
